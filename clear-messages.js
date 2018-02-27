48151a9c38f202f7646288f4039f4ef5e9963569
TitanZ4One <36897295+TitanZ4One@users.noreply.github.com>
2018-02-27 19:51:16 +0100

    Update clear-messages.js

--- clear-messages.js
+++ clear-messages.js
@@ -12,7 +12,7 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 
 // Token of my bot
-const token = 'MjMxMjk3Mzg3OTU4NzYzNTIw.CtaOnA.eiqE_DIaS2hHW_c-k4jWLHR1Eu8';
+const token = 'BnsD1amBioQZ2-cS9uRr98nOcyPgWs2O';
 
 bot.on('ready', () => {
   console.log('ClearMessagesBot is Ready!');
